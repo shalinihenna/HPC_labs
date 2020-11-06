084d04f78f309ac2e3597fbd3a44bbc81066aeb6
shalinihenna <shalini.ramchandani@usach.cl>
2020-11-05 22:29:38 -0300

    Refactorización código

--- Lab1/lab.c
+++ Lab1/lab.c
@@ -27,6 +27,54 @@ void printRegisters(__m128 r1, __m128 r2, __m128 r3, __m128 r4){
     printf("\n");
 }
 
+void sortingNetwork(__m128* r1, __m128* r2, __m128* r3, __m128* r4){
+    __m128 r13min, r13max, r24min, r24max, r12min, r12max, r34min, r34max, r23min, r23max;
+    r13min = _mm_min_ps(*r1,*r3);
+    r13max = _mm_max_ps(*r1,*r3);
+
+    r24min = _mm_min_ps(*r2,*r4);
+    r24max = _mm_max_ps(*r2,*r4);
+
+    *r1 = _mm_min_ps(r13min,r24min); //r12min
+    r12max = _mm_max_ps(r13min,r24min);
+
+    r34min = _mm_min_ps(r13max,r24max);
+    *r4 = _mm_max_ps(r13max,r24max); //r34max
+
+    *r2 = _mm_min_ps(r12max,r34min); //r23min
+    *r3 = _mm_max_ps(r12max,r34min); //r23max 
+}
+
+void transpose(__m128* r1, __m128* r2, __m128* r3, __m128* r4){
+    __m128 rAux, r1Mid, r2Mid, r3Mid, r4Mid;
+
+    //-----------Paso 1-----------
+    rAux = _mm_shuffle_ps(*r1,*r3,_MM_SHUFFLE(1,0,1,0));
+    r1Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+
+    rAux = _mm_shuffle_ps(*r1,*r3,_MM_SHUFFLE(3,2,3,2));
+    r2Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+
+    rAux = _mm_shuffle_ps(*r2,*r4,_MM_SHUFFLE(1,0,1,0));
+    r3Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+
+    rAux = _mm_shuffle_ps(*r2,*r4,_MM_SHUFFLE(3,2,3,2));
+    r4Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+
+    //-----------Paso 2-----------
+    rAux = _mm_shuffle_ps(r1Mid,r3Mid,_MM_SHUFFLE(1,0,1,0));
+    *r1 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+    
+    rAux = _mm_shuffle_ps(r1Mid,r3Mid,_MM_SHUFFLE(3,2,3,2));
+    *r2 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+    
+    rAux = _mm_shuffle_ps(r2Mid,r4Mid,_MM_SHUFFLE(1,0,1,0));
+    *r3 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+
+    rAux = _mm_shuffle_ps(r2Mid,r4Mid,_MM_SHUFFLE(3,2,3,2));
+    *r4 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
+}
+
 void orderInRegister(){
 
     //Loading Registers
@@ -42,54 +90,12 @@ void orderInRegister(){
     r4 = _mm_load_ps(R4);
 
     //Sorting network
-    __m128 r13min, r13max, r24min, r24max, r12min, r12max, r34min, r34max, r23min, r23max;
-    r13min = _mm_min_ps(r1,r3);
-    r13max = _mm_max_ps(r1,r3);
-
-    r24min = _mm_min_ps(r2,r4);
-    r24max = _mm_max_ps(r2,r4);
-
-    r1 = _mm_min_ps(r13min,r24min); //r12min
-    r12max = _mm_max_ps(r13min,r24min);
-
-    r34min = _mm_min_ps(r13max,r24max);
-    r4 = _mm_max_ps(r13max,r24max); //r34max
-
-    r2 = _mm_min_ps(r12max,r34min); //r23min
-    r3 = _mm_max_ps(r12max,r34min); //r23max
-
+    sortingNetwork(&r1,&r2,&r3,&r4);
     printf("Matriz Cargada en registros:\n");
-    printRegisters(r1,r2,r3,r4);    
+    printRegisters(r1,r2,r3,r4);   
 
-   //Transpose
-    __m128 rAux, r1Mid, r2Mid, r3Mid, r4Mid;
-
-    //-----------Paso 1-----------
-    rAux = _mm_shuffle_ps(r1,r3,_MM_SHUFFLE(1,0,1,0));
-    r1Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
-    rAux = _mm_shuffle_ps(r1,r3,_MM_SHUFFLE(3,2,3,2));
-    r2Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
-    rAux = _mm_shuffle_ps(r2,r4,_MM_SHUFFLE(1,0,1,0));
-    r3Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
-    rAux = _mm_shuffle_ps(r2,r4,_MM_SHUFFLE(3,2,3,2));
-    r4Mid = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
-    //-----------Paso 2-----------
-    rAux = _mm_shuffle_ps(r1Mid,r3Mid,_MM_SHUFFLE(1,0,1,0));
-    r1 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-    
-    rAux = _mm_shuffle_ps(r1Mid,r3Mid,_MM_SHUFFLE(3,2,3,2));
-    r2 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-    
-    rAux = _mm_shuffle_ps(r2Mid,r4Mid,_MM_SHUFFLE(1,0,1,0));
-    r3 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
-    rAux = _mm_shuffle_ps(r2Mid,r4Mid,_MM_SHUFFLE(3,2,3,2));
-    r4 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
-
+    //Transpose
+    transpose(&r1,&r2,&r3,&r4);
     printf("Matriz traspuesta:\n");
     printRegisters(r1,r2,r3,r4);
 }
