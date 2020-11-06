e2594f13b7ef1bf123038fe7c08b66e06c557493
shalinihenna <shalini.ramchandani@usach.cl>
2020-11-05 23:38:49 -0300

    BMN Listo

--- Lab1/lab.c
+++ Lab1/lab.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 #include <mmintrin.h> /* MMX instrinsics __m64 integer type */
 #include <xmmintrin.h> /* SSE __m128 float */
-//#include <emmintrin.h>
 
 int s[16] = {12, 21, 4, 13, 9, 8, 6, 7, 1, 14, 3, 0, 5, 11, 15, 10};
 
-void printList(float registers[4]){
-    printf("%f %f %f %f\n",registers[0],registers[1],registers[2],registers[3]);
+void printRegister(__m128 r1){
+    float R1[4] __attribute__((aligned(16)));
+    _mm_store_ps(R1,r1);
+    printf("%f %f %f %f\n",R1[0],R1[1],R1[2],R1[3]);
 }
 
 void printRegisters(__m128 r1, __m128 r2, __m128 r3, __m128 r4){
-    float R1[4] __attribute__((aligned(16)));
-    float R2[4] __attribute__((aligned(16)));
-    float R3[4] __attribute__((aligned(16)));
-    float R4[4] __attribute__((aligned(16)));
-
-    _mm_store_ps(R1,r1);
-    _mm_store_ps(R2,r2);
-    _mm_store_ps(R3,r3);
-    _mm_store_ps(R4,r4);
-
-    printList(R1);
-    printList(R2);
-    printList(R3);
-    printList(R4);
+    printRegister(r1);
+    printRegister(r2);
+    printRegister(r3);
+    printRegister(r4);
     printf("\n");
 }
 
@@ -75,6 +66,39 @@ void transpose(__m128* r1, __m128* r2, __m128* r3, __m128* r4){
     *r4 = _mm_shuffle_ps(rAux,rAux,_MM_SHUFFLE(3,1,2,0));
 }
 
+void bmn(__m128* r1, __m128* r2){
+    __m128 r1Aux, r2Aux, r1Aux1, r2Aux2;
+    
+    *r2 = _mm_shuffle_ps(*r2,*r2,_MM_SHUFFLE(0,1,2,3)); //Se invierte
+
+    //Primera etapa
+    r1Aux = _mm_shuffle_ps(*r1,*r1,_MM_SHUFFLE(3,1,2,0));
+    r2Aux = _mm_shuffle_ps(*r2,*r2,_MM_SHUFFLE(3,1,2,0));
+
+    *r1 = _mm_min_ps(r1Aux, r2Aux);
+    *r2 = _mm_max_ps(r1Aux, r2Aux);
+
+    //Segunda etapa
+    r1Aux = _mm_shuffle_ps(*r1,*r2,_MM_SHUFFLE(2,0,2,0));
+    r2Aux = _mm_shuffle_ps(*r1,*r2,_MM_SHUFFLE(3,1,3,1));
+
+    r1Aux = _mm_shuffle_ps(r1Aux,r1Aux,_MM_SHUFFLE(3,1,2,0));
+    r2Aux = _mm_shuffle_ps(r2Aux,r2Aux,_MM_SHUFFLE(3,1,2,0));
+
+    *r1 = _mm_min_ps(r1Aux, r2Aux);
+    *r2 = _mm_max_ps(r1Aux, r2Aux);
+
+    //Tercera etapa
+    r1Aux = _mm_shuffle_ps(*r1,*r2,_MM_SHUFFLE(1,0,1,0));
+    r2Aux = _mm_shuffle_ps(*r1,*r2,_MM_SHUFFLE(3,2,3,2));
+
+    r1Aux = _mm_shuffle_ps(r1Aux,r1Aux,_MM_SHUFFLE(3,1,2,0));
+    r2Aux = _mm_shuffle_ps(r2Aux,r2Aux,_MM_SHUFFLE(3,1,2,0));
+
+    *r1 = _mm_min_ps(r1Aux, r2Aux);
+    *r2 = _mm_max_ps(r1Aux, r2Aux);
+}
+
 void orderInRegister(){
 
     //Loading Registers
@@ -98,24 +122,14 @@ void orderInRegister(){
     transpose(&r1,&r2,&r3,&r4);
     printf("Matriz traspuesta:\n");
     printRegisters(r1,r2,r3,r4);
+
+    //Bitonic Merge Network (BMN)
+    bmn(&r1,&r2);
+    bmn(&r3,&r4);
+    printf("BMN\n");
+    printRegisters(r1,r2,r3,r4);
 }
 
 void main(){
     orderInRegister();
-    /*for(int i = 0; i < 16; i++){
-        printf("List! %d\n",s[i]);
-    }*/
-    /*
-    float R1[4] __attribute__((aligned(16))) = {0,1,2,3};
-    float R2[4] __attribute__((aligned(16))) = {8,9,10,11};
-    __m128 r1,r2,r3,r4;
-    r1 = _mm_load_ps(R1);
-    r2 = _mm_load_ps(R2);
-
-    r3 = _mm_shuffle_ps(r1,r2,_MM_SHUFFLE(1,0,1,0));
-    r4 = _mm_shuffle_ps(r3,r3,_MM_SHUFFLE(3,1,2,0));
-
-    _mm_store_ps(R1,r4);
-    printList(R1);
-*/
 }
