8f378c9363f64b2a4442d98e03928ae2cadcf640
shalinihenna <shalini.ramchandani@usach.cl>
2020-11-21 22:27:09 -0300

    Delete from heap done

--- Lab2/prueba.c
+++ Lab2/prueba.c
@@ -9,7 +9,6 @@ struct Heap
     int lengthArray;
 };
 
-//Verificar si la lista inicial pueden inicializarse con valores menores que 0.
 float arregloBacan[2][16] = {{12, 21, 4, 13, 9, 8, 6, 7, 1, 14, 3, 0, 5, 11, 15, 10}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}};
 float arreglo[16] = {12, 21, 4, 13, 9, 8, 6, 7, 1, 14, 3, 0, 5, 11, 15, 10};
 float bacan[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
@@ -23,12 +22,6 @@ void insertHeap(float value, struct Heap *heap)
     heap->lastPosition = heap->lastPosition + 1;
     sonIndex = heap->lastPosition;
     heap->array[sonIndex] = value;
-    printf("INICIO\n");
-    for (int k = 0; k < 16; k++)
-    {
-        printf("%f - ", heap->array[k]);
-    }
-    printf("\n\n");
 
     //Se reordena el heap
     fatherIndex = (int)((round(sonIndex / 2.0)) - 1);
@@ -38,79 +31,75 @@ void insertHeap(float value, struct Heap *heap)
         auxValue = heap->array[fatherIndex];
         heap->array[fatherIndex] = heap->array[sonIndex];
         heap->array[sonIndex] = auxValue;
-        printf("WHILE\n");
-        for (int k = 0; k < 16; k++)
-        {
-            printf("%f - ", heap->array[k]);
-        }
-        printf("\n\n");
 
         sonIndex = fatherIndex;
         fatherIndex = (int)((round(sonIndex / 2.0)) - 1);
     }
 }
 
-void deleteHeap(float *heap, int lengthHeap)
-{
-    int fatherIndex, firstSonIndex, secondSonIndex;
+void swap(struct Heap *heap, int *fatherIndex, int sonIndex){
     float auxValue;
+    auxValue = heap->array[*fatherIndex]; 
+    heap->array[*fatherIndex] = heap->array[sonIndex];
+    heap->array[sonIndex] = auxValue;
+    *fatherIndex = sonIndex;
+}
 
-    //Esta malo e imprimir la lista final
-    heap[0] = heap[lengthHeap - 1];
-    heap[lengthHeap - 1] = -1;
+void deleteHeap(struct Heap *heap){
+    int fatherIndex, firstSonIndex, secondSonIndex;
+
+    heap->array[0] = heap->array[heap->lastPosition];
+    heap->array[heap->lastPosition] = 0;
+    heap->lastPosition = heap->lastPosition - 1;
 
     fatherIndex = 0;
     firstSonIndex = 2 * fatherIndex + 1;
     secondSonIndex = 2 * fatherIndex + 2;
-
-    while ((fatherIndex < lengthHeap && (firstSonIndex < lengthHeap || secondSonIndex < lengthHeap)) && ((heap[fatherIndex] > heap[firstSonIndex]) || (heap[fatherIndex] > heap[secondSonIndex])))
-    {
-        printf("LISTA\n");
-        for (int k = 0; k < 16; k++)
-        {
-            printf("%f - ", heap[k]);
-        }
-        printf("\n");
-
-        //Para ver el mínimo
-        if ((heap[secondSonIndex] == -1 && heap[firstSonIndex] >= 0) || (heap[firstSonIndex] < heap[secondSonIndex]))
-        {
-            auxValue = heap[fatherIndex];
-            heap[fatherIndex] = heap[firstSonIndex];
-            heap[firstSonIndex] = auxValue;
-
-            fatherIndex = firstSonIndex;
-        }
-        else if ((heap[firstSonIndex] == -1 && heap[secondSonIndex] >= 0) || (heap[firstSonIndex] > heap[secondSonIndex]))
-        {
-            auxValue = heap[fatherIndex];
-            heap[fatherIndex] = heap[secondSonIndex];
-            heap[secondSonIndex] = auxValue;
-
-            fatherIndex = secondSonIndex;
+    
+    while((heap->array[fatherIndex] > heap->array[firstSonIndex] || heap->array[fatherIndex] > heap->array[secondSonIndex]) && (firstSonIndex <= heap->lastPosition || secondSonIndex <= heap->lastPosition)){
+       /*printf("father %d, value: %f \n", fatherIndex, heap->array[fatherIndex]); 
+       printf("son1 %d, value: %f\n", firstSonIndex, heap->array[firstSonIndex]);
+       printf("son2 %d, value: %f\n", secondSonIndex, heap->array[secondSonIndex]);
+       printf("last position: %d\n\n", heap->lastPosition);*/
+       if(firstSonIndex <= heap->lastPosition && secondSonIndex > heap->lastPosition){
+            if(heap->array[fatherIndex] > heap->array[firstSonIndex]){
+                swap(heap, &fatherIndex, firstSonIndex);
+            }else{
+                break;
+            }
+        }else if(secondSonIndex <= heap->lastPosition){
+            if(heap->array[fatherIndex] > heap->array[firstSonIndex] && heap->array[fatherIndex] > heap->array[secondSonIndex]){
+                if(heap->array[firstSonIndex] <= heap->array[secondSonIndex]){
+                    swap(heap, &fatherIndex, firstSonIndex);
+                }else if(heap->array[firstSonIndex] > heap->array[secondSonIndex]){
+                    swap(heap, &fatherIndex, secondSonIndex);
+                }        
+            }else if(heap->array[fatherIndex] > heap->array[firstSonIndex] && heap->array[fatherIndex] < heap->array[secondSonIndex]){
+                swap(heap, &fatherIndex, firstSonIndex);
+            }else if(heap->array[fatherIndex] < heap->array[firstSonIndex] && heap->array[fatherIndex] > heap->array[secondSonIndex]){
+                swap(heap, &fatherIndex, secondSonIndex);
+            }else{
+                break;
+            }
         }
         firstSonIndex = 2 * fatherIndex + 1;
         secondSonIndex = 2 * fatherIndex + 2;
     }
 }
 
-void assignFirstElement(float *heap, float *finalList, int lengthFinalList, int lengthHeap)
+void assignFirstElement(struct Heap *heap, float *finalList, int lengthFinalList)
 {
     for (int i = 0; i < lengthFinalList; i++)
     {
         if (finalList[i] == -1)
         {
-            finalList[i] = heap[0];
-            deleteHeap(heap, lengthHeap);
+            finalList[i] = heap->array[0];
+            deleteHeap(heap);
             i = lengthFinalList;
         }
     }
 }
 
-void multiWaySort(int length)
-{
-    int lengthList = length / 16;
-}
 
 void main()
 {
@@ -127,20 +116,31 @@ void main()
     //Hacer el for por cada elemento, llamando al insert en el heap
     for (int j = 0; j < 16; j++)
     {
-        insertHeap(bacan[j], &heap);
+        insertHeap(arreglo[j], &heap);
     }
-
+    printf("LISTA\n");
     for (int k = 0; k < 16; k++)
     {
         printf("%f - ", heap.array[k]);
     }
     printf("\n");
 
-    /*assignFirstElement(heap, finalList, 16, 16);
-    printf("NUEVA LISTA\n");
+    for(int i = 0; i < 16; i++){
+        assignFirstElement(&heap, finalList, 16);
+    }
+    
+    printf("NUEVA heap\n");
     for (int k = 0; k < 16; k++)
     {
-        printf("%f - ", heap[k]);
+        printf("%f - ", heap.array[k]);
     }
-    printf("\n");*/
+    printf("\n");
+
+    printf("NUEVA lista final\n");
+    for (int k = 0; k < 16; k++)
+    {
+        printf("%f - ", finalList[k]);
+    }
+    printf("\n");
+
 }
